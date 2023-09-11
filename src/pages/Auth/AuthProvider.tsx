079c34bc5ee1426568f1fd09a5b7b2c9e5949700
akuznetsov <akuznetsov@t1-consulting.ru>
2023-09-11 20:58:05 +0400

    retrello_router: Убран закоментированный код

--- src/pages/Auth/AuthProvider.tsx
+++ src/pages/Auth/AuthProvider.tsx
@@ -1,6 +1,5 @@
-import {ReactNode, useState} from 'react';
-//обернуть в ErrorBoundary почитать и обернуть
-import {AuthContext} from '../../context/AuthContext';
+import { ReactNode, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 type AuthType = {
   isLoggedIn: boolean;
@@ -9,7 +8,6 @@ type AuthType = {
 
 export const AuthProvider = ({ children }: { children: ReactNode }): any => {
   const [auth, setAuth] = useState<AuthType | null>(null);
-  // получаем данные авторизирован ли пользователь, засовываем их в контекст
   const authenticate = (userName: string) => {
     setAuth({ isLoggedIn: true, userName: userName });
   };
@@ -21,28 +19,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }): any => {
   const signout = () => {
     setAuth(null);
   };
-  return <AuthContext.Provider value={{ isLoggedIn, signout, authenticate, userName }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, signout, authenticate, userName }}>{children}</AuthContext.Provider>
+  );
 };
-
-// export const useAuth = () => {
-//   const [user, setUser] = useState<string | null>(null);
-//   const navigate = useNavigate();
-
-//   const login = (userName: string) => {
-//     setUser(userName);
-//     navigate('/table', { replace: true });
-//   };
-
-//   const isLoggedIn = !!user;
-
-//   const exit = () => {
-//     setUser(null);
-//     navigate('/auth', { replace: true });
-//   };
-
-//   const getUser = () => {
-//     return user;
-//   };
-
-//   return { getUser, login, exit, isLoggedIn };
-// };

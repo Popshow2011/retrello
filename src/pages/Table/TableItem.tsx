4f39248e338b35d7db8469c69585e680313bb6df
akuznetsov <akuznetsov@nota.tech>
2023-12-05 12:20:53 +0400

    fix

--- src/pages/Table/TableItem.tsx
+++ src/pages/Table/TableItem.tsx
@@ -1,6 +1,5 @@
 import { ColumnType, TodoType } from '@/types';
 import { TodoItem } from '@/components';
-import { useEffect } from 'react';
 
 type ItemProps = {
   tableItem: ColumnType;
@@ -9,20 +8,18 @@ type ItemProps = {
 
 export const TableItem = ({ tableItem, todos }: ItemProps) => {
   return (
-    <>
-      <div className=" rounded bg-grey-light  flex-no-shrink w-64 p-2 mr-3">
-        <div className="flex justify-between py-1">
-          <h3 className="text-2xl font-bold text-center">{tableItem.name}</h3>
-        </div>
-        <div className="text-sm mt-2">
-          {todos?.length &&
-            todos.map((item) => (
-              <div key={item.id}>
-                <TodoItem todo={item} />
-              </div>
-            ))}
-        </div>
+    <div className=" rounded bg-grey-light  flex-no-shrink w-64 p-2 mr-3">
+      <div className="flex justify-between py-1">
+        <h3 className="text-2xl font-bold text-center">{tableItem.name}</h3>
       </div>
-    </>
+      <div className="text-sm mt-2">
+        {todos?.length &&
+          todos.map((item) => (
+            <div key={item.id}>
+              <TodoItem todo={item} />
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };

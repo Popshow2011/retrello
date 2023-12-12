2ee3874b80305ba442d41438fa72c73b61e9d55b
Алексей Кузнецов <alexey@Mac-mini-Aleksej.local>
2023-12-13 01:10:53 +0400

    Добавлен в проект react-hook-form изменена логика работы с формой

--- src/pages/Table/NewTodo.tsx
+++ src/pages/Table/NewTodo.tsx
@@ -1,29 +1,25 @@
 import { Outlet, useNavigate } from 'react-router-dom';
-import { ChangeEvent, useState } from 'react';
 import { useMutation } from 'react-query';
 import axios from 'axios';
-import { TodoType } from '@/types';
+import { useForm } from 'react-hook-form';
 
 export const NewTodo = () => {
   const navigate = useNavigate();
-  const [newTodo, setNewTodo] = useState<TodoType>({
-    completed: false,
-    id: 0,
-    title: '',
-    userId: 0,
-    tableId: 0,
-  });
+  const { register, handleSubmit } = useForm();
 
   const closeModal = () => {
     return navigate('/');
   };
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewTodo({ ...newTodo, title: event.target.value });
-  };
-
   const { mutate, isLoading } = useMutation({
-    mutationFn: () => axios.post('https://jsonplaceholder.typicode.com/todos', newTodo),
+    mutationFn: (title: string) =>
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        completed: false,
+        id: 0,
+        title,
+        userId: 0,
+        tableId: 0,
+      }),
     onSuccess: (data) => {
       console.log('TODO успешно добавлен:', data);
       navigate('/');
@@ -44,33 +40,34 @@ export const NewTodo = () => {
           <div className="relative  bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="px-6 py-6 lg:px-8">
               <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Добавление нового TODO</h3>
-              <div className="space-y-6">
-                <div>
-                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Наименование TODO
-                  </label>
-                  <input
-                    name="name"
-                    onChange={handleChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required
-                  />
-                </div>
-                <button
-                  onClick={() => mutate()}
-                  disabled={isLoading}
-                  className={`w-full text-white bg-blue-700 disabled:border-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
-                >
-                  Добавить
-                </button>
+              <form onSubmit={handleSubmit(({ title }) => mutate(title))}>
+                <div className="space-y-6">
+                  <div>
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Наименование TODO
+                    </label>
+                    <input
+                      {...register('title')}
+                      className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-gray-900"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    disabled={isLoading}
+                    className={`w-full text-white bg-blue-700 disabled:border-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                  >
+                    Добавить
+                  </button>
 
-                <button
-                  onClick={closeModal}
-                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Отменить
-                </button>
-              </div>
+                  <button
+                    onClick={closeModal}
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Отменить
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
